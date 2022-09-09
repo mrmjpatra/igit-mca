37174397ad5e69040c749ca4af0723cb7c8926c7
mrmjpatra <mrmjpatra@gmail.com>
2022-09-09 13:29:46 +0530

    Add About Section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,10 @@
-
+import React from 'react'
+import AchievementSection from '../../components/AchievementSection'
+import '../../components/achievement.css'
 const About = () => {
   return (
     <div>
-      
+      <AchievementSection/>
     </div>
   )
 }
